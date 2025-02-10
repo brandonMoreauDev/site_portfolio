a780dc0b50f0aeeec861de1c31a6f55b5719f6c6
Moreaubw <brandon.m.dev@gmail.com>
2025-02-10 09:53:47 +0100

    sv

---
+++ index.js
@@ -0,0 +1,21 @@
+let lastScroll = 0;
+const navbar = document.getElementById("navbar"); // Utilisation directe de l'id
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY < lastScroll) {
+    navbar.style.top = 0;
+  } else {
+    navbar.style.top = "-68px";
+  }
+
+  lastScroll = window.scrollY;
+});
+
+function telechargerFichier() {
+  const lien = document.createElement("a");
+  lien.href = "./assets/doc/CV_Brandon.pdf";
+  lien.download = "CV_Brandon";
+  document.body.appendChild(lien);
+  lien.click();
+  document.body.removeChild(lien);
+}
