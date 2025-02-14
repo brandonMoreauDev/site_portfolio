a297d5fdf2934c9d5b5f99d332eef384b2633e84
Moreaubw <brandon.m.dev@gmail.com>
2025-02-14 16:49:50 +0100

    sv

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ window.addEventListener("scroll", () => {
 
 function telechargerFichier() {
   const lien = document.createElement("a");
-  lien.href = "./assets/doc/CV_Brandon.pdf";
+  lien.href = "./assets/doc/CV_Moreau_Brandon.pdf";
   lien.download = "CV_Moreau_Brandon";
   document.body.appendChild(lien);
   lien.click();
